935ea041ceef961ba847af7515d5555c7186a6c7
Ryannoonan <ryan@qevlar.com>
2025-07-22 22:21:53 +0200

    minimal frontend races

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
-import { ChangeEventHandler, useEffect, useRef, useState } from "react"
+import { createContext, ReactElement, useEffect, useRef, useState } from "react"
+import { TestComponent } from "./test"
+import React from "react"
 
 const dummyTexts = [
   "I am the fastest typer on the planet.",
@@ -7,23 +9,36 @@ const dummyTexts = [
   "Pizza is high on fat and carbs."
 ]
 
+
 export default function Home() {
-  const [textValue, setTextValue] = useState("")
+
+  const [currentTypedWord, setCurrentTypedWord] = useState("")
   const [raceStarted, setRaceStarted] = useState(false)
   const inputRef = useRef<HTMLInputElement>(null)
   const [timeOfPrevInput, setTimeOfPrevInput] = useState(Date.now())
-  const [textToType, setTextToType] = useState("")
-
+  const textToType = useRef("")
+  const [correctWords, setCorrectWords] = useState("")
+  const [[correctText, incorrectText, remainingText], setWordSplit] = useState(["", "", ""])
 
   function OnTextChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setTextValue(event.target.value);
+    let currentTypedWord = event.target.value
+    const currentTypedText = correctWords + currentTypedWord
     console.log(Date.now());
     console.log("time passed: ", Date.now() - timeOfPrevInput)
     setTimeOfPrevInput(Date.now())
+    if (GetTextDivision(textToType.current, currentTypedText)[2].length == 0) { 
+      alert("You won!")
+    }
+    setWordSplit(GetTextDivision(textToType.current, currentTypedText))
+    if (currentTypedWord.charAt(currentTypedWord.length - 1) === " " && GetTextDivision(textToType.current,currentTypedText)[1].length === 0) {
+      setCorrectWords(currentTypedText)
+      currentTypedWord = ""
+    }
+    setCurrentTypedWord(currentTypedWord);
   }
-
   function StartRace() {
-    setTextToType(GetRandomTextToType())
+    textToType.current = GetRandomTextToType()
+    setWordSplit(["","", textToType.current])
     setRaceStarted(true);
     setTimeOfPrevInput(Date.now())
     if (inputRef.current) {
@@ -33,24 +48,39 @@ export default function Home() {
       console.error("Unable to focus on textInput")
     }
   }
-
   function GetRandomTextToType() {
     const idx = Math.floor(Math.random() * dummyTexts.length)
     return dummyTexts[idx]
   }
 
+
+  function GetTextDivision(textToType: string, typedText: string): [string, string, string] {
+    function GetSplittingIndices(textToType: string, typedText: string) {
+      let idx1 = -1
+      while (idx1 + 1 < Math.min(textToType.length, typedText.length) && textToType[idx1 + 1] === typedText[idx1 + 1]) {
+        idx1++;
+      }
+      let idx2 = Math.min(textToType.length - 1, typedText.length - 1)
+      return [idx1, idx2]
+    }
+    const [idx1, idx2] = GetSplittingIndices(textToType, typedText)
+    return [textToType.slice(0, idx1 + 1), textToType.slice(idx1 + 1, idx2 + 1), textToType.slice(idx2 + 1, textToType.length)]
+  }
   return (
     <>
-      <input
-        ref={inputRef}
-        id="textbox"
-        value={textValue}
-        onChange={OnTextChange}
-        readOnly={!raceStarted}
-      />
-      <div> Here is the value {textValue} </div>
-      <button onClick={StartRace}> click to start race </button >
-      <div> Text to type: {textToType} </div>
-    </>
+    <input
+      ref={inputRef}
+      id="textbox"
+      value={currentTypedWord}
+      onChange={OnTextChange}
+      readOnly={!raceStarted}
+    />
+    <div> Text to type: <span style={{ color: "blue" }}>{correctText}</span><span style={{ backgroundColor: "red" }}>{incorrectText}</span><span style={{ color: "black" }}>{remainingText}</span></div>
+    <button onClick={StartRace}> click to start race</button >
+    <div> <span>test</span><span>hello</span></div>
+
+    
+  </>
   )
 }
+
